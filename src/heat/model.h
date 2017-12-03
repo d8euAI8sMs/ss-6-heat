4146d5ba147866bd9b4d49954f62613e17516a46
kalaider <a.kalaider@yandex.ru>
2017-12-03 17:07:42 +0300

    [c] Some problem-specific definitions added

---
+++ src/heat/model.h
@@ -0,0 +1,38 @@
+#pragma once
+
+namespace model
+{
+
+    struct parameters
+    {
+        // cylinder params
+        double R, L, d, lambda_m;
+
+        // ring params
+        double d_c, L_c, z_c;
+
+        // heat source params
+        double R_h, L_h, z_h, P_h;
+
+        // other params
+        double dt;
+    };
+
+    inline static parameters make_default_parameters()
+    {
+        return
+        {
+            // cylinder params
+            2, 1, 0.05, 10,
+
+            // ring params
+            0.05, 0.2, 0.5,
+
+            // heat source params
+            0.5, 0.1, 0.2, 1,
+
+            // other params
+            0.01
+        };
+    }
+}
