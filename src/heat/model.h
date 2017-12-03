0c1c144345e6a4bae55673541b1f4fa8051623a0
kalaider <a.kalaider@yandex.ru>
2017-12-03 20:26:06 +0300

    [c] Default parameters updated

--- src/heat/model.h
+++ src/heat/model.h
@@ -27,13 +27,13 @@ namespace model
         return
         {
             // cylinder params
-            2, 1, 0.05, 10,
+            1, 2.5, 0.05, 10,
 
             // ring params
             0.05, 0.2, 0.5,
 
             // heat source params
-            0.5, 0.1, 0.2, 1,
+            0.5, 0.1, 0.5, 1,
 
             // other params
             0.01
