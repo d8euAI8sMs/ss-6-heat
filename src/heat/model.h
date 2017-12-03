1b3994cd9fe82cf8180ddb78140afe05a573d7d4
kalaider <a.kalaider@yandex.ru>
2017-12-03 18:38:11 +0300

    [c] Plotting-specific definitions added

--- src/heat/model.h
+++ src/heat/model.h
@@ -1,7 +1,11 @@
 #pragma once
 
+#include "plot.h"
+
 namespace model
 {
+
+    using points_t = std::vector < plot::point < double > > ;
 
     struct parameters
     {
@@ -34,5 +38,39 @@ namespace model
             // other params
             0.01
         };
+    }
+
+    struct plot_data
+    {
+        util::ptr_t < std::vector < points_t > > data;
+        plot::multilist_drawable < points_t > :: ptr_t plot;
+        plot::world_t world;
+        plot::world_mapper_t world_mapper;
+    };
+
+    inline static plot_data make_plot_data()
+    {
+        plot_data pd;
+        pd.data = util::create < std::vector < points_t > > ();
+        pd.plot = plot::multilist_drawable < points_t > :: create
+        (
+            plot::make_data_source(pd.data),
+            nullptr // no point painter
+        );
+        pd.world_mapper = plot::make_world_mapper(pd.world);
+        return pd;
+    }
+
+    inline static void adjust(parameters & params,
+                              plot_data  & data)
+    {
+        data.world.xmin = data.world.ymin = 0;
+        data.world.xmax = params.L + 2 * params.d;
+        data.world.ymax = params.R + params.d;
+        if (params.z_c > 0)
+        {
+            data.world.ymax += params.d_c;
+        }
+        params.dr = params.dz = min(params.d_c, params.d) / 2;
     }
 }
