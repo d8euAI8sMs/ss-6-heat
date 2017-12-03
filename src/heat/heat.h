b74eef938b19e79b686d55a2028883e52174b94c
kalaider <a.kalaider@yandex.ru>
2017-12-03 14:20:16 +0300

    [c] The configured solution with template project created

---
+++ src/heat/heat.h
@@ -0,0 +1,32 @@
+
+// heat.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CheatApp:
+// See heat.cpp for the implementation of this class
+//
+
+class CheatApp : public CWinApp
+{
+public:
+	CheatApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CheatApp theApp;
